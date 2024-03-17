ff77d4924e1c172a42c7864a0333bd3df32ec69e
hasibiqbal207 <hasibiqbal207@gmail.com>
2024-03-17 23:02:32 +0100

    Custom logger added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
 import app from "./app";
 import dotenv from "dotenv";
+import logger from "./configs/logger.config";
 
 // dotenv config
 dotenv.config();
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => logger.info(`Server running on port ${PORT}`));
