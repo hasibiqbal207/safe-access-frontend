021c966c4a8b9967be6193248645acd6d19447d2
hasibiqbal207 <hasibiqbal207@gmail.com>
2024-03-20 01:11:22 +0100

    Router & Redux Storesetup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 const App = () => {
   return (
-    <div>Hello</div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Register />} />
+      </Routes>
+    </Router>
   )
 }
 
